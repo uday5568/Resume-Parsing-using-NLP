0c28ac7e23e3d4dd4026e9a33cd95d1567287f67
Gangadhar C <gangadharganga90@gmail.com>
2023-03-26 00:16:31 +0530

    login and sign update v1

--- Code/main-app.py
+++ Code/main-app.py
@@ -2,6 +2,8 @@ import mysql.connector
 from tkinter import *
 import hashlib
 import re
+
+
 # ws = Tk()
 # ws.geometry("1080x1080")
 # ws.state('zoomed')
@@ -130,27 +132,25 @@ import re
 class MainWindow():
 
     def __init__(self, mainWidget):
-        self.root=mainWidget
+        self.root = mainWidget
         self.root.state('zoomed')
-        self.gui_elements=[]
+        self.gui_elements = []
         self.root['bg'] = '#012'
         mydb = mysql.connector.connect(host="localhost", user="root")
         db = mydb.cursor()
         try:
             sql = "CREATE DATABASE IF NOT EXISTS resumesparser"
             db.execute(sql)
-            self.login_gui()
+
         except Exception as e:
             errmsg = Label(self.root, text="*Database Creation Failed", font=('Arial', 12), fg='#ff0000',
                            bg='#012', width=30)
             errmsg.place(x=700, y=430)
-            self.root.after(3000, errmsg.destroy)
+        finally:
+            self.login_gui()
 
         mydb.commit()
         mydb.close()
-
-
-
 
     def login_gui(self):
         self.root.title('Login Page')
@@ -165,103 +165,136 @@ class MainWindow():
         self.gui_elements.append(Label(self.root, text='Login', width=15, font=('Arial', 18)))
         self.gui_elements[-1].place(x=login_x + 150, y=login_y - 70)
 
-        self.gui_elements.append(Label(self.root, text="Username", font=('Arial', 16), width=10))
+        self.gui_elements.append(
+            Label(self.root, text="Username", fg="#fff", bg="#012", font=('Arial', 16, "bold"), width=10))
         self.gui_elements[-1].place(x=login_x, y=login_y)
         self.gui_elements.append(Entry(self.root, width=25, font=('Arial 16'), textvariable=username_l))
         self.gui_elements[-1].place(x=login_x + 150, y=login_y)
-        self.gui_elements.append(Label(self.root, text="Password", font=('Arial', 16), width=10))
+        self.gui_elements.append(
+            Label(self.root, text="Password", fg="#fff", bg="#012", font=('Arial', 16, "bold"), width=10))
         self.gui_elements[-1].place(x=login_x, y=login_y + 50)
         self.gui_elements.append(Entry(self.root, width=25, font=('Arial 16'), textvariable=password_l))
         self.gui_elements[-1].place(x=login_x + 150, y=login_y + 50)
         self.gui_elements.append(Label(self.root, text="New User?", font=('Arial', 13), fg="#fff", bg="#012", width=10))
         self.gui_elements[-1].place(x=login_x + 150,
-                                                                                                     y=login_y + 90)
+                                    y=login_y + 90)
 
-        self.gui_elements.append(Button(self.root, text="Create Account", font=('Arial 12'), fg="#00f", bg="#012", bd="0",
-               command=self.signup_gui,
-               width=12, height=1))
+        self.gui_elements.append(
+            Button(self.root, text="Create Account", font=('Arial 12'), fg="#00f", bg="#012", bd="0",
+                   command=self.signup_gui,
+                   width=12, height=1))
         self.gui_elements[-1].place(x=login_x + 245, y=login_y + 87)
 
-        self.gui_elements.append(Button(self.root, text="Login", font=('Arial 12'), command=self.back_to_main, width=10, height=1))
-        self.gui_elements[-1].place(x=login_x + 200,y=login_y + 130)
-
-
+        self.gui_elements.append(
+            Button(self.root, text="Login", font=('Arial 12'), command=self.login_gui, width=10, height=1))
+        self.gui_elements[-1].place(x=login_x + 200, y=login_y + 130)
 
     def signup_gui(self):
         self.root.title('SignUp Page')
         self.gui_elements_remove(self.gui_elements)
         username_s = StringVar()
+        email_s = StringVar()
         password_s = StringVar()
+        re_password_s = StringVar()
+        signup_x = 500
+        signup_y = 350
+
         def fun():
             mydb = mysql.connector.connect(host="localhost", user="root", database='resumesparser')
             db = mydb.cursor()
             try:
-                sql = "SELECT username FROM users"
+                sql = '''CREATE TABLE IF NOT EXISTS `users` (
+                  `username` varchar(11) NOT NULL ,   
+                  `email` varchar(250) NOT NULL ,       
+                  `password` varchar(30)  NOT NULL ,     
+                   PRIMARY KEY  (`username`)
+                );'''
                 db.execute(sql)
-                if str(hashlib.md5(username_s.get().encode()).hexdigest()) in [i[0] for i in db]:
-                    errmsg = Label(self.root, text="*Username Taken", font=('Arial', 12), fg='#ff0000', bg='#012', width=20)
-                    errmsg.place(x=700, y=350)
-                    self.root.after(3000, errmsg.destroy)
-                else:
-                    sql = "INSERT INTO users VALUES(MD5(%s),MD5(%s))"
-                    val = (username_s.get(), password_s.get())
-                    if re.search('[A-Z]', val[1]) and re.search('[a-z]', val[1]) and re.search('[0-9]',
-                                                                                               val[1]) and re.search(
-                            '[@_!#$%^&*()<>?/\|}{~:]', val[1]) and len(val[1]) >= 8:
-                        db.execute(sql, val)
-                        errmsg = Label(self.root, text="*Registration Successful", font=('Arial', 12), fg='#00ff00',
-                                       bg='#012', width=30)
-                        errmsg.place(x=700, y=350)
-                        self.root.after(3000, errmsg.destroy)
 
-                    else:
-                        errmsg = Label(self.root, text="*Weak Password Try Again", font=('Arial', 12), fg='#ff0000',
-                                       bg='#012', width=30)
-                        errmsg.place(x=700, y=350)
-                        self.root.after(3000, errmsg.destroy)
 
             except Exception as e:
-                errmsg = Label(self.root, text="*Registration Failed, Error Occured", font=('Arial', 12), fg='#ff0000',
-                               bg='#012', width=30)
-                errmsg.place(x=700, y=430)
-                self.root.after(3000, errmsg.destroy)
+                self.message("*Table Creation Failed, Error Occured", x=signup_x + 200, y=signup_y + 80)
+
+            try:
+                if (
+                        username_s.get() and email_s.get() and password_s.get() and password_s.get() == re_password_s.get()):
+                    sql = "SELECT username FROM users"
+                    db.execute(sql)
+                    if str(hashlib.md5(username_s.get().encode()).hexdigest()) in [i[0] for i in db]:
+                        raise Exception('*Username Taken')
+                    else:
+                        sql = "INSERT INTO users VALUES(MD5(%s),%s,MD5(%s))"
+                        val = (username_s.get(), email_s.get(), password_s.get())
+                        if re.search('[A-Z]', val[1]) and re.search('[a-z]', val[1]) and re.search('[0-9]',
+                                                                                                   val[
+                                                                                                       1]) and re.search(
+                            '[@_!#$%^&*()<>?/\|}{~:]', val[1]) and len(val[1]) >= 8:
+                            db.execute(sql, val)
+                            self.message("*Registration Successful", x=signup_x + 200, y=signup_y, color='#00ff00')
+
+
+                        else:
+                            self.message('*Weak Password Try Again', x=signup_x + 150, y=signup_y + 130)
+
+                else:
+                    if (not username_s.get()):
+                        self.message('Username Empty', x=signup_x + 150, y=signup_y + 28)
+                    if (not email_s.get()):
+                        self.message('Email Empty', x=signup_x + 150, y=signup_y + 78)
+                    if (not password_s.get()):
+                        self.message('password Empty', x=signup_x + 150, y=signup_y + 128)
+                    elif (password_s.get() != re_password_s.get()):
+                        self.message('*Password Don\'t match', x=signup_x + 150, y=signup_y + 180)
+            except Exception as e:
+                print(e)
+                self.message("*Resgitration Failed,Error Occured", x=signup_x + 150, y=signup_y + 180)
 
             mydb.commit()
             mydb.close()
 
-        signup_x = 500
-        signup_y = 350
         self.gui_elements.append(Label(self.root, text='Resume Parser Using NLP', font=('Arial', 30)))
         self.gui_elements[-1].place(x=signup_x, y=signup_y - 170)
         self.gui_elements.append(Label(self.root, text='SignUp', width=15, font=('Arial', 18)))
         self.gui_elements[-1].place(x=signup_x + 150, y=signup_y - 70)
 
-        self.gui_elements.append(Label(self.root, text="Username", font=('Arial', 16), width=10))
+        self.gui_elements.append(
+            Label(self.root, text="Username", fg="#fff", bg="#012", font=('Arial', 16, "bold"), width=10))
         self.gui_elements[-1].place(x=signup_x, y=signup_y)
         self.gui_elements.append(Entry(self.root, width=25, font=('Arial 16'), textvariable=username_s))
         self.gui_elements[-1].place(x=signup_x + 150, y=signup_y)
-        self.gui_elements.append(Label(self.root, text="Password", font=('Arial', 16), width=10))
+
+        self.gui_elements.append(
+            Label(self.root, text="Email", fg="#fff", bg="#012", font=('Arial', 16, "bold"), width=10))
         self.gui_elements[-1].place(x=signup_x, y=signup_y + 50)
-        self.gui_elements.append(Entry(self.root, width=25, font=('Arial 16'), textvariable=password_s))
+        self.gui_elements.append(Entry(self.root, width=25, font=('Arial 16'), textvariable=email_s))
         self.gui_elements[-1].place(x=signup_x + 150, y=signup_y + 50)
+
+        self.gui_elements.append(
+            Label(self.root, text="Password", fg="#fff", bg="#012", font=('Arial', 16, "bold"), width=10))
+        self.gui_elements[-1].place(x=signup_x, y=signup_y + 100)
+        self.gui_elements.append(Entry(self.root, width=25, font=('Arial 16'), textvariable=password_s))
+        self.gui_elements[-1].place(x=signup_x + 150, y=signup_y + 100)
+
+        self.gui_elements.append(
+            Label(self.root, text="Re-type Password", fg="#fff", bg="#012", font=('Arial', 16, "bold"), width=14))
+        self.gui_elements[-1].place(x=signup_x - 70, y=signup_y + 150)
+        self.gui_elements.append(Entry(self.root, width=25, font=('Arial 16'), textvariable=re_password_s))
+        self.gui_elements[-1].place(x=signup_x + 150, y=signup_y + 150)
+
         self.gui_elements.append(Button(self.root, text="SignUp", font=('Arial 12'), command=fun, width=10, height=1))
-        self.gui_elements[-1].place(x=signup_x + 200,y=signup_y + 130)
+        self.gui_elements[-1].place(x=signup_x + 200, y=signup_y + 230)
         self.root.mainloop()
 
-
-
-
-
-    def back_to_main(self):
-        if self.some_kind_of_controler == 1:
-            self.gui_elements_remove(self.gui_elements)
-        else:
-            pass
-        self.main_gui()
+    def message(self, msg, x=0, y=0, color='#ff0000'):
+        errmsg = Label(self.root, text=msg, font=('Arial', 10), fg=color,
+                       bg='#012', width=30)
+        errmsg.place(x=x, y=y)
+        self.root.after(3000, errmsg.destroy)
 
     def gui_elements_remove(self, elements):
         for element in elements:
             element.destroy()
+
 
 def main():
     root = Tk()
@@ -269,6 +302,7 @@ def main():
     window = MainWindow(root)
 
     root.mainloop()
+
 
 if __name__ == '__main__':
     main()
