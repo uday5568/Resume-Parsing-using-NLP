43c81667dc51537685a2b6fb0a2b426609634365
uday <pinnantiuday@gmail.com>
2023-03-26 12:41:41 +0530

    skillset update

--- experiments.py
+++ experiments.py
@@ -1,3 +1,12 @@
+## Use to upload files in Google colab
+#from google.colab import files
+#uploaded = files.upload()
+
+
+
+# pip install docx2txt
+# pip install pypdf2
+import os
 import win32com.client as win32
 from win32com.client import constants
 from pdfminer.converter import TextConverter
@@ -6,47 +15,21 @@ from pdfminer.pdfinterp import PDFResourceManager
 from pdfminer.layout import LAParams
 from pdfminer.pdfpage import PDFPage
 import docx2txt
-from pdfminer.converter import TextConverter
-from pdfminer.pdfinterp import PDFPageInterpreter
-from pdfminer.pdfinterp import PDFResourceManager
-from pdfminer.layout import LAParams
-from pdfminer.pdfpage import PDFPage
-import docx2txt
+# import doc2text
 from PyPDF2 import PdfFileReader, PdfFileWriter, PdfFileMerger, PdfReader
 import io
-import os
-from email.message import EmailMessage
-import ssl
-import smtplib
-import pandas as pd
-import spacy
-import json
-nlp = spacy.load('en_core_web_sm')
 
-
-import re
-import nltk
-nltk.download('stopwords')
-from nltk.corpus import stopwords
-stop = stopwords.words('english')
-nltk.download('punkt')
-nltk.download('averaged_perceptron_tagger')
-nltk.download('maxent_ne_chunker')
-nltk.download('words')
-import spacy
-import en_core_web_sm
-from nltk.tag.stanford import StanfordNERTagger
-from spacy.matcher import Matcher
-from nameparser.parser import HumanName
-from nltk.corpus import wordnet
-
-
+#Extracting text from DOCX
 def doctotext(m):
     temp = docx2txt.process(m)
     resume_text = [line.replace('\t', ' ') for line in temp.split('\n') if line]
     text = ' '.join(resume_text)
     return (text)
 
+
+
+
+## Extracting text from PDF
 def pdftotext(m):
     # pdf file object
     # you can find find the pdf file with complete code in below
@@ -118,6 +101,22 @@ def pdftotext(m):
   
 
 
+import re
+import nltk
+nltk.download('stopwords')
+from nltk.corpus import stopwords
+stop = stopwords.words('english')
+nltk.download('punkt')
+nltk.download('averaged_perceptron_tagger')
+nltk.download('maxent_ne_chunker')
+nltk.download('words')
+import spacy
+import en_core_web_sm
+from nltk.tag.stanford import StanfordNERTagger
+from spacy.matcher import Matcher
+from nameparser.parser import HumanName
+from nltk.corpus import wordnet
+
 # load pre-trained model
 nlp = en_core_web_sm.load()
 
@@ -129,7 +128,7 @@ def extract_name(resume_text):
     nlp_text = nlp(resume_text)
     
     # First name and Last name are always Proper Nouns
-    pattern = [{'POS': 'PROPN'}, {'POS': 'PROPN'}]
+    pattern = [{'POS': 'PROPN'}, {'POS': 'PROPN'}, {'POS': 'PROPN'}]
     
     matcher.add('NAME', None, pattern)
     
@@ -140,7 +139,8 @@ def extract_name(resume_text):
         return span.text
 
 
-
+import re
+from nltk.corpus import stopwords
 
 
 # Grad all general stop words
@@ -150,7 +150,7 @@ STOPWORDS = set(stopwords.words('english'))
 EDUCATION = [
             'BE','B.E.', 'B.E','(B.E)', 'BS', 'B.S', 
             'ME', 'M.E', 'M.E.', 'M.B.A', 'MBA', 'MS', 'M.S', 
-            'BTECH', 'B.TECH', 'M.TECH', 'MTECH','B-TECH','HIGHER SECONDARY SCHOOL','SECONDARY SCHOOL',
+            'BTECH', 'B.TECH', 'M.TECH', 'MTECH', 'B-TECH','HIGHER SECONDARY SCHOOL','SECONDARY SCHOOL',
             'SSLC', 'SSC' 'HSC', 'CBSE', 'ICSE', 'X', 'XII','BACHELOR','ENGINEERING','12TH','10TH','INTERMEDIATE','SSC'
         ]
 
@@ -166,7 +166,7 @@ def extract_education(resume_text):
         for tex in text.split():
             # Replace all special symbols
             tex = re.sub(r'[?|$|.|!|,|)|(]', r'', tex)
-            if index+1< len(nlp_text) and tex.upper() in EDUCATION and tex not in STOPWORDS:
+            if tex.upper() in EDUCATION and tex not in STOPWORDS:
                 edu[tex] = text + nlp_text[index + 1]
                 
                 
@@ -182,7 +182,7 @@ def extract_education(resume_text):
     return education
 BRANCH=[
     'CHEMICAL','MINING','AERONAUTICAL','TEXTILE','MECHATRONICS','CIVIL',
-    'ELECTRONICS','COMMUNICATION','ROBOTICS','POWER','AEROSPACE','MECHANICAL',
+    'ELECTRONICS','ROBOTICS','POWER','AEROSPACE','MECHANICAL',
     'STRUCTURAL','INDUSTRIAL','MARINE','PETROLEUM','AUTOMOBILE','PRODUCTION',
     'METALLURGICAL','CERAMIC','BIOMEDICAL','CONSTRUCTION','ELECTRONICS','TOOL'
     ,'TELECOMMUNICATION','ENVIRONMENTAL','TRANSPORTATION','ELECTRONICS AND COMMUNICATION'
@@ -200,7 +200,7 @@ def extract_branch(resume_text):
         for tex in text.split():
             # Replace all special symbols
             tex = re.sub(r'[?|$|.|!|,|)|(]', r'', tex)
-            if index+1< len(nlp_text) and tex.upper() in BRANCH and tex not in STOPWORDS:
+            if tex.upper() in BRANCH and tex not in STOPWORDS:
                 edu[tex] = text + nlp_text[index + 1]
                 
 
@@ -214,23 +214,26 @@ def extract_branch(resume_text):
             education.append(key)
     return education
 
-
+import pandas as pd
+import spacy
+nlp = spacy.load('en_core_web_sm')
 
 # noun_chunks = nlp.noun_chunks
 noun_chunks=None
 def extract_skills(resume_text):
-    global noun_chunks,skill_set
+    global noun_chunks
     nlp_text = nlp(resume_text)
     noun_chunks=nlp_text.noun_chunks
     # removing stop words and implementing word tokenization
     tokens = [token.text for token in nlp_text if not token.is_stop]
+    # print(tokens)
+    colnames = ['skill']
     # reading the csv file
-    data = pd.read_csv(f'{skill_set}.csv') 
+    data = pd.read_csv('skill.csv', names=colnames) 
     
     # extract values
-    skills = data['skill'].tolist()
+    skills = data.skill.tolist()
     skillset = []
-    print(skills)
     
     # check for one-grams (example: python)
     for token in tokens:
@@ -257,6 +260,10 @@ def extract_mobile_number(resume_text):
 
 
 
+def extract_CGPA(string):
+    r = re.compile(r'(?:(?:\s[1-9][0-9]{1}(?:\.[0-9]+)*\s?%|\s[1-9][0-9]{1}(?:\.[0-9]+)*\s)|(?:\s[1-9]\.[0-9]+\s\.?|(?:\s10.[0]+\s|\s10\s)))') 
+    return r.findall((string))
+
 def extract_email_addresses(string):
     r = re.compile(r'[\w\.-]+@[\w\.-]+')
     return r.findall(string)[0] if r.findall(string) else ''
@@ -272,7 +279,7 @@ def extract_github_addresses(string):
 
 
 
-
+import json
  
 
  
@@ -285,18 +292,18 @@ def data_display():
     'Mobile Number':extract_mobile_number(textinput),
     'Email':extract_email_addresses(textinput),
     'LinkedIn':extract_linkedin_addresses(textinput),
-    'Github':extract_github_addresses(textinput)
+    'Github':extract_github_addresses(textinput),
+    'CGPA':extract_CGPA(textinput)
     }
     json_object = json.dumps(out_data, indent=4)
     
-    with open(f"output/{re.sub('[^A-Za-z]','',out_data['Name'])}_{skill_set}_result.json", "w") as outfile:
+    with open(f"{re.sub('[^A-Za-z]','',out_data['Name'])} result.json", "w") as outfile:
         outfile.write(json_object)
         outfile.close()
     print(json_object)
-    return len(out_data['Skills']),out_data['Email']
 def save_as_docx(path):
     # Opening MS Word
-    word = win32.Dispatch("SAPI.SpVoice")
+    word = win32.Dispatch()
     doc = word.Documents.Open(path)
     doc.Activate ()
 
@@ -310,64 +317,20 @@ def save_as_docx(path):
     )
     doc.Close(False)
 
-def sendMails(email_sender='pinnantiuday@gmail.com',email_pass="mzfypoqkatxnqbdc",email_recevier=['pinnantiuday@gmail.com'],skill_set='python'):
-    subject="You have been Shortlisted"
-    body=f'''
-    Hi, congratulations your resume has been shortlisted for {skill_set} role.
-
-    This is a Testing message for Resume parsing for HR analytics project done by Uday from BE-CSE-SIST
-    Kindly Ignore This Mail...
-    '''
-    em=EmailMessage()
-    em['From']=email_sender
-    em['To']=email_recevier
-    em['Subject']=subject
-    em.set_content(body)
-    context=ssl.create_default_context()
-    with smtplib.SMTP_SSL('smtp.gmail.com',465,context=context) as smtp:
-        smtp.login(email_sender,email_pass)
-        for mailadd in email_recevier:
-            if(re.match(r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,7}\b',mailadd)):
-                smtp.sendmail(email_sender,mailadd,em.as_string())
-            else:
-                print('Error sending mail to :',mailadd)
-    print("Mails Successfully Sent")
-                
-
 if __name__ == '__main__': 
-    scores={}
-    skill_set=input("Enter Domain : ")
-    for file in os.listdir(r'C:\Users\pinna\Jupyter notebooks\Resumes'):
-        try:
-            # FilePath = input().replace("\'",'').replace('\"','')
-            # FilePath=r"C:\Users\pinna\Downloads\SIST-BE-CSE-39110202-ChakaliGangadhar.pdf"
-            FilePath='C:/Users/pinna/Jupyter notebooks/Resumes/'+file
-            if FilePath.endswith('.docx'):
-                textinput = doctotext(FilePath)
-                y=data_display()
-                scores[file]=y
-            elif FilePath.endswith('.doc'):
-                save_as_docx(FilePath)
-                textinput = doctotext(FilePath+'x')
-                y=data_display()
-                scores[file]=y
-            elif FilePath.endswith('.pdf'):
-                textinput = pdftotext(FilePath)
-                score,mail=data_display()
-                scores[file]=[score,mail]
-            else:
-                print("File not support")
-        except Exception as e:
-            print(e)
-    df = pd.DataFrame(columns=["File",'Email','scores'])
- 
-    for fname in sorted(scores,key=lambda x:scores[x][0],reverse=True):
-        df.loc[len(df.index)] = [fname,scores[fname][1],int(scores[fname][0])]
-    df.to_csv('Ranks.csv',index=False)
-    rank_lim=int(input(f'Rank Limit (max : {max(scores.values(),key=lambda x:x[0])[0]}): '))
-    mails=[]
-    for fname in sorted(scores,key=lambda x:scores[x][0],reverse=True):
-        if(int(scores[fname][0])>=rank_lim):
-            mails.append(scores[fname][1])
-    print(*mails)
-    #sendMails(email_recevier=mails,skill_set=skill_set)
+
+    # FilePath = input().replace("\'",'').replace('\"','')
+    # FilePath.lower().endswith(('.png', '.docx'))
+    FilePath=r"C:\Users\pinna\Jupyter notebooks\Resumes\React JS Developer_AnjaniPriyadarshini.doc"
+    if FilePath.endswith('.docx'):
+      textinput = doctotext(FilePath)
+      data_display()
+    elif FilePath.endswith('.doc'):
+      save_as_docx(FilePath)
+      textinput = doctotext(FilePath+'x')
+      data_display()
+    elif FilePath.endswith('.pdf'):
+        textinput = pdftotext(FilePath)
+        data_display()
+    else:
+      print("File not support")
--- skills.py
+++ skills.py
@@ -1,5 +1,5 @@
 import pandas as pd
-import spacy
+# import spacy
 # nlp = spacy.load('en_core_web_sm')
 # noun_chunks = nlp.noun_chunks
 
@@ -28,9 +28,19 @@ import spacy
 #     return [i.capitalize() for i in set([i.lower() for i in skillset])]
   
 # print ('Skills:',extract_skills(textinput))
-df=pd.DataFrame(["assembly", "bash", " c " "c++", "c#", "coffeescript", "emacs lisp",
-         "go!", "groovy", "haskell", "java", "javascript", "matlab", "max MSP", "objective c", 
-         "perl", "php","html", "xml", "css", "processing", "python", "ruby", "sml", "swift", 
-         "latex" "unity", "unix","visual basic", "wolfram language", "xquery", "sql", "node.js",
-         "scala", "kdb", "jquery", "mongodb","r","machine learning","data analysis","data science","artificial intelligence","deep learning"],columns=['skill'])
-df.to_csv('skill.csv',index=False)
+
+
+# df=pd.DataFrame(["assembly", "bash", " c " "c++", "c#", "coffeescript", "emacs lisp",
+#          "go!", "groovy", "haskell", "java", "javascript", "matlab", "max MSP", "objective c", 
+#          "perl", "php","html", "xml", "css", "processing", "python", "ruby", "sml", "swift", 
+#          "latex" "unity", "unix","visual basic", "wolfram language", "xquery", "sql", "node.js",
+#          "scala", "kdb", "jquery", "mongodb","r","machine learning","data analysis","data science","artificial intelligence","deep learning"],columns=['skill'])
+# df.to_csv('skill.csv',index=False)
+
+
+df=pd.DataFrame({"skill":[ "java", "javascript","node.js","css","jquery", "sql","xml","json","web","framework"],
+                 "Score":[4,3,2,1,2,4,3,4,2,1]})
+df.to_csv('java.csv',index=False)
+# df=pd.DataFrame({"skill":["deep learning","python","data science","machine learning","artificial intelligence", "flask","html","css"],
+#                 "Score":[3,4,5,4,4,2,2,2]})
+# df.to_csv('python.csv',index=False)
